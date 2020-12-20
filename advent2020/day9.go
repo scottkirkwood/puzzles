d4b2b47c69ac9170d9676ebcbf4eb8d4b0f3a426
Scott Kirkwood <scott@forusers.com>
2020-12-19 22:15:57 -0500

    Now in 4 dimensions

--- advent2020/day9.go
+++ advent2020/day9.go
@@ -18,16 +18,16 @@ type world struct {
 }
 
 type minMax struct {
-	min vec3
-	max vec3
+	min vec4
+	max vec4
 }
 
 type cube struct {
-	w map[vec3]bool
+	w map[vec4]bool
 }
 
-type vec3 struct {
-	x, y, z int
+type vec4 struct {
+	x, y, z, w int
 }
 
 // String is the stringer interface for world
@@ -37,15 +37,18 @@ func (w world) String() string {
 
 func (w *world) cycle() {
 	w.next = newCube()
-	coord := vec3{}
-	for z := w.extents.min.z - 1; z <= w.extents.max.z+1; z++ {
-		coord.z = z
-		for y := w.extents.min.y - 1; y <= w.extents.max.y+1; y++ {
-			coord.y = y
-			for x := w.extents.min.x - 1; x <= w.extents.max.x+1; x++ {
-				coord.x = x
-				if w.cur.alive(coord) {
-					w.next.set(coord, true)
+	coord := vec4{}
+	for w1 := w.extents.min.w - 1; w1 <= w.extents.max.w+1; w1++ {
+		coord.w = w1
+		for z := w.extents.min.z - 1; z <= w.extents.max.z+1; z++ {
+			coord.z = z
+			for y := w.extents.min.y - 1; y <= w.extents.max.y+1; y++ {
+				coord.y = y
+				for x := w.extents.min.x - 1; x <= w.extents.max.x+1; x++ {
+					coord.x = x
+					if w.cur.alive(coord) {
+						w.next.set(coord, true)
+					}
 				}
 			}
 		}
@@ -57,22 +60,24 @@ func (w *world) cycle() {
 
 func newCube() cube {
 	return cube{
-		w: make(map[vec3]bool),
+		w: make(map[vec4]bool),
 	}
 }
 
-func (c *cube) alive(coord vec3) bool {
+func (c *cube) alive(coord vec4) bool {
 	count := 0
-	for z := -1; z <= 1; z++ {
-		for y := -1; y <= 1; y++ {
-			for x := -1; x <= 1; x++ {
-				if x == 0 && y == 0 && z == 0 {
-					continue
-				}
-				if c.get(vec3{coord.x + x, coord.y + y, coord.z + z}) {
-					count++
-					if count > 3 {
-						break
+	for w := -1; w <= 1; w++ {
+		for z := -1; z <= 1; z++ {
+			for y := -1; y <= 1; y++ {
+				for x := -1; x <= 1; x++ {
+					if x == 0 && y == 0 && z == 0 && w == 0 {
+						continue
+					}
+					if c.get(vec4{coord.x + x, coord.y + y, coord.z + z, coord.w + w}) {
+						count++
+						if count > 3 {
+							break
+						}
 					}
 				}
 			}
@@ -88,15 +93,18 @@ func (c *cube) alive(coord vec3) bool {
 func (c *cube) countActive() int {
 	count := 0
 	extents := c.calcExtents()
-	coord := vec3{}
-	for z := extents.min.z; z <= extents.max.z; z++ {
-		coord.z = z
-		for y := extents.min.y; y <= extents.max.y; y++ {
-			coord.y = y
-			for x := extents.min.x; x <= extents.max.x; x++ {
-				coord.x = x
-				if c.get(coord) {
-					count++
+	coord := vec4{}
+	for w := extents.min.w; w <= extents.max.w; w++ {
+		coord.w = w
+		for z := extents.min.z; z <= extents.max.z; z++ {
+			coord.z = z
+			for y := extents.min.y; y <= extents.max.y; y++ {
+				coord.y = y
+				for x := extents.min.x; x <= extents.max.x; x++ {
+					coord.x = x
+					if c.get(coord) {
+						count++
+					}
 				}
 			}
 		}
@@ -104,11 +112,11 @@ func (c *cube) countActive() int {
 	return count
 }
 
-func (c *cube) set(coord vec3, val bool) {
+func (c *cube) set(coord vec4, val bool) {
 	c.w[coord] = val
 }
 
-func (c *cube) get(coord vec3) bool {
+func (c *cube) get(coord vec4) bool {
 	return c.w[coord]
 }
 
@@ -116,9 +124,11 @@ func minMaxMaxout() (m minMax) {
 	m.min.x = 999
 	m.min.y = 999
 	m.min.z = 999
+	m.min.w = 999
 	m.max.x = -999
 	m.max.y = -999
 	m.max.z = -999
+	m.max.w = -999
 	return m
 }
 
@@ -135,6 +145,9 @@ func (c cube) calcExtents() minMax {
 			if k.z < m.min.z {
 				m.min.z = k.z
 			}
+			if k.w < m.min.w {
+				m.min.w = k.w
+			}
 			if k.x > m.max.x {
 				m.max.x = k.x
 			}
@@ -143,6 +156,9 @@ func (c cube) calcExtents() minMax {
 			}
 			if k.z > m.max.z {
 				m.max.z = k.z
+			}
+			if k.w > m.max.w {
+				m.max.w = k.w
 			}
 		}
 	}
@@ -153,18 +169,20 @@ func (c cube) calcExtents() minMax {
 func (c cube) String() string {
 	extents := c.calcExtents()
 	lines := []string{}
-	for z := extents.min.z; z <= extents.max.z; z++ {
-		lines = append(lines, fmt.Sprintf("\nz=%d", z))
-		for y := extents.min.y; y <= extents.max.y; y++ {
-			line := ""
-			for x := extents.min.x; x <= extents.max.x; x++ {
-				if c.get(vec3{x, y, z}) {
-					line += "#"
-				} else {
-					line += "."
+	for w := extents.min.w; w <= extents.max.w; w++ {
+		for z := extents.min.z; z <= extents.max.z; z++ {
+			lines = append(lines, fmt.Sprintf("\nz=%d, w=%d", z, w))
+			for y := extents.min.y; y <= extents.max.y; y++ {
+				line := ""
+				for x := extents.min.x; x <= extents.max.x; x++ {
+					if c.get(vec4{x, y, z, w}) {
+						line += "#"
+					} else {
+						line += "."
+					}
 				}
+				lines = append(lines, line)
 			}
-			lines = append(lines, line)
 		}
 	}
 	return strings.Join(lines, "\n")
@@ -180,7 +198,7 @@ func read(fname string) (cube, error) {
 
 	scanner := bufio.NewScanner(file)
 
-	coord := vec3{}
+	coord := vec4{}
 	for scanner.Scan() {
 		line := scanner.Text()
 		coord.x = 0
