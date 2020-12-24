e21fc425c6311fc6adfdc7043b21269bfadb8905
Scott Kirkwood <scott@forusers.com>
2020-12-24 15:29:58 -0500

    Completed day23 part 2

--- advent2020/day23.go
+++ advent2020/day23.go
@@ -18,148 +18,213 @@ var testFileFlag = flag.Bool("t", false, "Use the test file")
 // The other points to the next lower number.  With these two changes
 // the algorithm will be fast
 type game struct {
-	moveNum int
-	pos     int
-	numbers []int
+	moveNum    int
+	countNodes int   // number of nodes in loop
+	start      *node // start of loop
+	pos        *node // current position, also used for add()
+	max        *node // node with maximum number value
+	min        *node // node with value 1
+}
+
+type node struct {
+	num  int
+	next *node
+	dec  *node
 }
 
 func (g game) String() string {
 	lines := []string{}
 	lines = append(lines, fmt.Sprintf("-- move %d --", g.moveNum))
 	lines = append(lines, fmt.Sprintf("cups: %s", g.formatCups()))
-	lines = append(lines, fmt.Sprintf("pick up: %s", joinInts(g.pickUp(), ", ")))
-	lines = append(lines, fmt.Sprintf("destination: %d", g.destination()))
+	pickup := g.pickupCopy()
+	lines = append(lines, fmt.Sprintf("pick up: %s", joinInts(pickup, ", ")))
+	// lines = append(lines, fmt.Sprintf("count down: %s", joinDecInts(g.max, ", ")))
+	lines = append(lines, fmt.Sprintf("destination: %d", g.findDestination(g.pos, pickup).num))
 	return strings.Join(lines, "\n")
 }
 
 func (g *game) move() {
-	prevVal := g.numbers[g.pos]
-	dest := g.destination()
-	pickup := g.pickUp()
-	newList := g.removeThese(pickup)
-	destIndex := indexOf(newList, dest)
-	g.numbers = insertAfter(newList, destIndex, pickup)
-
-	g.pos = indexOf(g.numbers, prevVal)
-	g.pos = g.inc(g.pos)
 	g.moveNum++
+	pickup, last := g.pickUp()
+	destination := g.findDestination(g.pos, pickup)
+	last.next = destination.next
+	destination.next = pickup
+	g.pos = g.pos.next
 }
 
-func (g game) removeThese(pickup []int) []int {
-	m := intToMap(pickup)
-	ret := make([]int, 0, len(g.numbers)-3)
-	for i := 0; i < len(g.numbers); i++ {
-		if m[g.numbers[i]] {
-			continue
-		}
-		ret = append(ret, g.numbers[i])
-	}
-	return ret
+func (g *game) init() {
+	g.pos = g.start
 }
 
-func insertAfter(lst []int, after int, toInsert []int) []int {
-	ret := make([]int, 0)
-	for i := 0; i <= after; i++ {
-		ret = append(ret, lst[i])
+func (g *game) add(num int) {
+	newNode := &node{
+		num: num,
 	}
-	for _, x := range toInsert {
-		ret = append(ret, x)
+	if num == 1 {
+		g.min = newNode
 	}
-	for i := after + 1; i < len(lst); i++ {
-		ret = append(ret, lst[i])
+	g.countNodes++
+	if g.start == nil {
+		g.start = newNode
+		g.pos = g.start
+		g.max = newNode
+		newNode.next = g.start
+		newNode.dec = nil
+		return
 	}
-	return ret
+	newNode.next = g.pos.next
+	g.pos.next = newNode
+	g.pos = newNode
+	if num > g.max.num {
+		newNode.dec = g.max
+		g.max = newNode
+	} else {
+		oneUp := g.findOneUp(num)
+		newNode.dec = oneUp.dec
+		oneUp.dec = newNode
+	}
 }
 
-func intToMap(nums []int) map[int]bool {
-	m := make(map[int]bool, len(nums))
-	for _, num := range nums {
-		m[num] = true
-	}
-	return m
-}
-
-func (g game) inc(index int) int {
-	return (index + 1) % len(g.numbers)
-}
-
-func (g game) afterOne() string {
-	ret := []string{}
-	index := indexOf(g.numbers, 1)
+func (g game) findOneUp(num int) *node {
+	p := g.max
+	prev := p
 	for {
-		index = g.inc(index)
-		if g.numbers[index] == 1 {
-			break
+		if p == nil || p.num < num {
+			return prev
 		}
-		ret = append(ret, fmt.Sprintf("%d", g.numbers[index]))
+		prev = p
+		p = p.dec
 	}
-	return strings.Join(ret, "")
+	return prev
 }
 
-func indexOf(numbers []int, num int) int {
-	for i, n := range numbers {
-		if n == num {
-			return i
-		}
+func (g game) afterOne(count int) {
+	p := g.min.next
+	prod := 1
+	for i := 0; i < count; i++ {
+		fmt.Printf("%d ", p.num)
+		prod *= p.num
+		p = p.next
 	}
-	return 0
+	fmt.Printf("\nProduct: %d\n", prod)
 }
 
 func (g game) formatCups() string {
 	ret := []string{}
-	for i, num := range g.numbers {
-		if i == g.pos {
-			ret = append(ret, fmt.Sprintf("(%d)", num))
+	p := g.start
+	i := 0
+	for {
+		if p == g.pos {
+			ret = append(ret, fmt.Sprintf("(%d)", p.num))
 		} else {
-			ret = append(ret, fmt.Sprintf("%d ", num))
+			ret = append(ret, fmt.Sprintf("%d ", p.num))
 		}
 		if i > 10 {
 			ret = append(ret, "...")
+			break
+		}
+		i++
+		p = p.next
+		if p == g.start || p == nil {
+			if p == nil {
+				fmt.Printf("Unexpected nil\n")
+			}
 			break
 		}
 	}
 	return strings.Join(ret, " ")
 }
 
-func (g game) pickUp() []int {
-	ret := make([]int, 3)
-	p := g.pos
+func (g *game) pickupCopy() *node {
+	p := g.pos.next
+
+	var start *node
+	var last *node
 	for i := 0; i < 3; i++ {
-		p = g.inc(p)
-		ret[i] = g.numbers[p]
+		cur := &node{
+			num: p.num,
+		}
+		if start == nil {
+			start = cur
+		} else {
+			last.next = cur
+		}
+		p = p.next
+		last = cur
 	}
-	return ret
+	return start
+}
+func (g *game) pickUp() (start, last *node) {
+	start = g.pos.next
+	last = start
+	for i := 0; i < 2; i++ {
+		last = last.next
+	}
+	// remove from loop
+	g.pos.next = last.next
+	last.next = nil
+	return start, last
 }
 
-func (g game) destination() int {
-	m := make(map[int]bool, 3)
-	for _, n := range g.pickUp() {
-		m[n] = true
+func (g game) findDestination(p *node, notIn *node) *node {
+	p = g.decrement(p)
+	for notIn.contains(p) {
+		p = g.decrement(p)
 	}
-	num := g.numbers[g.pos]
+	return p
+}
+
+func (g game) decrement(p *node) *node {
+	p = p.dec
+	if p == nil {
+		return g.max
+	}
+	return p
+}
+
+func (n *node) contains(other *node) bool {
+	p := n
 	for {
-		num--
-		if num <= 0 {
-			num = len(g.numbers)
+		if p.num == other.num {
+			return true
 		}
-		if !m[num] {
+		p = p.next
+		if p == nil || p == n {
+			return false
+		}
+	}
+	return false
+}
+
+func joinInts(start *node, joiner string) string {
+	ret := []string{}
+	p := start
+	for {
+		ret = append(ret, strconv.FormatInt(int64(p.num), 10))
+		p = p.next
+		if p == nil || p == start {
 			break
 		}
 	}
-	return num
+	return strings.Join(ret, joiner)
 }
 
-func joinInts(nums []int, joiner string) string {
-	ret := make([]string, len(nums))
-	for i, n := range nums {
-		ret[i] = strconv.FormatInt(int64(n), 10)
+func joinDecInts(start *node, joiner string) string {
+	ret := []string{}
+	p := start
+	for {
+		ret = append(ret, strconv.FormatInt(int64(p.num), 10))
+		p = p.dec
+		if p == nil {
+			break
+		}
 	}
 	return strings.Join(ret, joiner)
 }
 
 var playerRx = regexp.MustCompile(`Player (\d):`)
 
-func read(fname string) (game, error) {
+func read(fname string, extra int) (game, error) {
 	ret := game{moveNum: 1}
 	file, err := os.Open(fname)
 	if err != nil {
@@ -174,10 +239,11 @@ func read(fname string) (game, error) {
 			continue
 		}
 		for _, ch := range line {
-			ret.numbers = append(ret.numbers, parseNum(string(ch)))
+			ret.add(parseNum(string(ch)))
 		}
-		for i := len(ret.numbers) + 1; i <= 1000000; i++ {
-			ret.numbers = append(ret.numbers, i)
+		maxVal := ret.max.num
+		for i := maxVal + 1; i <= extra; i++ {
+			ret.add(i)
 		}
 	}
 	if err := scanner.Err(); err != nil {
@@ -201,16 +267,21 @@ func main() {
 	fmt.Printf("Day 23\n")
 
 	infile := "day23.input"
+	extra := 1000000
 	if *testFileFlag {
 		infile = "day23-sample.input"
+		//extra = 0
 	}
-	game, err := read(infile)
+	game, err := read(infile, extra)
 	if err != nil {
 		fmt.Printf("Err: %v\n", err)
 		return
 	}
-	for i := 0; i < 100; i++ {
+	game.init()
+	moves := 10000000
+	for i := 0; i < moves; i++ {
+		//fmt.Printf("%s\n", game.String())
 		game.move()
 	}
-	//fmt.Printf("%s\n", game.afterOne())
+	game.afterOne(2)
 }
